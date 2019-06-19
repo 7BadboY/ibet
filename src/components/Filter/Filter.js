e5882ec8060e9ae458ff5032697e4f7281234d81
AleksandraHrevtsova <47608805+AleksandraHrevtsova@users.noreply.github.com>
2019-06-19 14:32:30 +0300

    upd

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import classes from './Filter.module.css';
 
@@ -23,12 +24,15 @@ const Filter = ({ onHandleChangeFilter, filter }) => {
       >
         In the game
       </Button>
-      <Button
+      {/* <Button
         onClick={() => onHandleChangeFilter('creatingDate')}
         disabled={filter === 'creatingDate'}
-      />
+      /> */}
     </div>
   );
 };
-
+Filter.propTypes = {
+  onHandleChangeFilter: PropTypes.func.isRequired,
+  filter: PropTypes.func.isRequired,
+};
 export default Filter;
